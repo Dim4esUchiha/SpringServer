4d1d728c913d67faa01dbcccae99fc5162da8e81
KEKWait <dimas_orlov99@mail.ru>
2023-04-13 12:06:17 +0300

    ER diagram MySQL and SQL queries for creation

--- main.sql
+++ main.sql
@@ -30,6 +30,80 @@ delete from person where id=4;
 insert into user(email, nickname, password) values('maxim@mail.ru', 'Maxim', 'Shadow_Fiend');
 
 
+create table Tag(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    tag_name varchar(30) unique
+);
+
+insert into Tag(tag_name) values("Cinema");
+select * from tag;
+
+create table Person_Tag(
+	person_id int,
+    tag_id int,
+    foreign key (person_id) references person(id),
+    foreign key (tag_id) references tag(id),
+    primary key (person_id, tag_id)
+);
+
+create table Data(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    file_path varchar(100),
+    file_type varchar(30),
+    person_id int,
+    foreign key (person_id) references person(id)
+);
+
+create table chat(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    chat_name varchar(50)
+);
+
+create table message(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date datetime,
+    info varchar(255),
+    chat_id int,
+    foreign key (chat_id) references chat(id)
+);
+
+create table user_chat(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    user_id int,
+    chat_id int,
+    foreign key (user_id) references user(id),
+    foreign key (chat_id) references chat(id)
+);
+
+create table role(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_name varchar(30), -- enum
+    person_id int ,
+    foreign key(person_id) references person(id)
+);
+
+create table contact(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    date_add datetime,
+    user_id int,
+    foreign key (user_id) references user(id)
+);
+
+
+
+
+
+
+drop table data;
+
+
+drop table person_tag;
+drop table tag;
+drop table person;
+drop table user;
+
+
+
 
 
 
